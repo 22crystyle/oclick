e2b710738f9fa8dc46ee926e66a4520c686a62f8
22crystyle <22crystyle@gmail.com>
2025-11-23 12:37:36 +0300

    Eureka Basic Configuration

--- platform/eureka-server/build.gradle.kts
+++ platform/eureka-server/build.gradle.kts
@@ -1,11 +1,6 @@
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
 group = "org.oclick.platform.eureka"
 version = "unspecified"
 
-tasks.test {
-    useJUnitPlatform()
+dependencies {
+    implementation(libs.spring.cloud.starter.eureka.server)
 }
