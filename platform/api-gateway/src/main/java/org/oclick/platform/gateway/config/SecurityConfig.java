29a1fa1d222deaec007e01b03c97e57baffc5d13
22crystyle <22crystyle@gmail.com>
2025-11-28 16:57:09 +0300

    SecurityConfig for api-gateway to bypass spring-security.

---
+++ platform/api-gateway/src/main/java/org/oclick/platform/gateway/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package org.oclick.platform.gateway.config;
+
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().permitAll()
+                )
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+                .build();
+    }
+
+    @Bean
+    public GlobalFilter customGlobalFilter() { // TODO: remove SecurityBreach
+        return ((exchange, chain) ->  {
+            exchange.getRequest().mutate()
+                    .header("X-Internal-Gateway", "true")
+                    .build();
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+
+            }));
+        });
+    }
+}
