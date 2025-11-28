068c2bb9fa358d6e4571c73718f154140c9ddaa1
22crystyle <22crystyle@gmail.com>
2025-11-28 14:45:45 +0300

    feat: api-gateway configuration

--- platform/api-gateway/build.gradle.kts
+++ platform/api-gateway/build.gradle.kts
@@ -2,5 +2,13 @@ group = "org.oclick.platform.gateway"
 version = "unspecified"
 
 dependencies {
-    implementation(libs.spring.cloud.starter.gateway)
+    implementation(libs.spring.cloud.starter.gateway.webflux)
+    implementation(libs.spring.boot.starter.webflux)
+    implementation(libs.spring.cloud.starter.loadbalancer)
+    implementation(libs.resilience4j)
+}
+
+configurations.implementation {
+    exclude(module = "spring-webmvc")
+    exclude(module = "spring-boot-starter-tomcat")
 }
